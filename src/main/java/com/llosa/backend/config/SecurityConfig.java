c8e43e60b655d86885944f6f9a47e983e8e6780e
Jose Daniel <jose.huaman@utec.edu.pe>
2026-05-24 19:58:49 -0500

    feat: url actualized and settled

--- src/main/java/com/llosa/backend/config/SecurityConfig.java
+++ src/main/java/com/llosa/backend/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "http://localhost:4200"
+                "http://localhost:4200",
+                "https://llosa.ingsoftware.lat"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
