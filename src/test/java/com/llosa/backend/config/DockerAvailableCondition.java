d378f0a48042638c7bc805cefc7719684af08761
tkaos <juan.ticlia@utec.edu.pe>
2026-06-10 23:24:21 -0500

    Revert "fix: logs para cofiguracion de test"
    This reverts commit afa56440cf651599982b3c23eb4074a9aae96522.

---
+++ src/test/java/com/llosa/backend/config/DockerAvailableCondition.java
@@ -0,0 +1,17 @@
+package com.llosa.backend.config;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class DockerAvailableCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        try {
+            return org.testcontainers.DockerClientFactory.instance().isDockerAvailable();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
