459d0b1d8323b9553c60f5eeeef9ea9ae6011aa3
Jose Daniel <jose.huaman@utec.edu.pe>
2026-06-18 12:29:49 -0500

    file: just PagoFLujoFActory

---
+++ src/main/java/com/llosa/backend/factory/PagoFlujoFactory.java
@@ -0,0 +1,14 @@
+package com.llosa.backend.factory;
+
+import com.llosa.backend.pagos.dto.CronogramaPagoRequest;
+import com.llosa.backend.pagos.entity.Pago;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PagoFlujoFactory {
+    public List<Pago> RealizarHitosPorCronograma(CronogramaPagoRequest request) {
+
+    }
+}
